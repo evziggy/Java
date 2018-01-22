30e9c14faa42a296a779211d947b04a876e8869d
Evense Zetrenne <ezetrenne@gmail.com>
2018-01-22 14:20:28 -0500

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class ArraySum {	
+
+public int Summation(int[][] myArray) {
+		
+int sum = 0;
+
+int num = 0;
+
+   for(int num : array) {
+   sum = sum+num;
+}
+
+return sum;
+
+}   
+
+
+public static void main (String[] args) {		
+
+java.util.Scanner input = new Scanner(System.in); 
+   
+int[10][2] matrix;
+   
+   System.out.println("Enter " + matrix.length + " rows and " +
+matrix[0].length + " columns: ");
+   
+   for (int row = 0; row < matrix.length; row++) {
+   
+      for (int column = 0; column < matrix[row]
+      [column] = input.nextInt();
+      } 
+ 
+   }
+}
---
+++ CarLoan.java
@@ -0,0 +1,34 @@
+/**************************************************************************/
+/* This program will calculate the monthly car payment a user should expect to make after taking out a car loan.
+/* Coded by: Evense Zetrenne
+/**************************************************************************/
+
+public class CarLoan {
+	public static void main(String[] args) {
+
+int carLoan = 10000;	
+int loanLength = 3;
+int intrestRate = 5;
+int downPayment = 2000;
+   
+if(loanLength <= 0 || intrestRate <= 0 ) {
+  
+  System.out.println("Error! You must take out a valid car loan.");
+}    
+  else if (downPayment >= carLoan) {
+
+			System.out.println("The car can be paid in full.");
+}
+   else {
+		
+    int remainingBalance = carLoan - downPayment; 
+		int months = loanLength*12;
+    int monthlyBalance = remainingBalance/months; 
+		int intrest = (monthlyBalance*intrestRate)/100;
+    int monthlyPayment = monthlyBalance+intrest;
+     
+     System.out.println(monthlyPayment);
+		}	 
+    
+	}
+}
